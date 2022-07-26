5677ecf3028b601777e85006b8ade33bfcb0f991
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-07-26 15:07:20 +0700

    RadioCheckbox

---
+++ selenium/webdriver/Topic_12_Custom_RadioCheckbox.java
@@ -0,0 +1,57 @@
+package webdriver;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_12_Custom_RadioCheckbox {
+	WebDriver driver;
+	String projectPath = System.getProperty("user.dir");
+	JavascriptExecutor jsExecutor;
+	
+	public void sleepSecond(long timeSleep) {
+		try {
+			Thread.sleep(timeSleep*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		jsExecutor = (JavascriptExecutor) driver;
+
+	}
+
+	@Test
+	public void TC_01_() {
+		driver.get("https://docs.google.com/forms/d/e/1FAIpQLSfiypnd69zhuDkjKgqvpID9kwO29UCzeCVrGGtbNPZXQok0jA/viewform");
+		
+		By canthoRadio = By.xpath("//div[@aria-label='Cần Thơ']");
+		//click by js
+		jsExecutor.executeScript("arguments[0].click();", canthoRadio);
+		
+		//verify click
+		Assert.assertEquals(driver.findElement(canthoRadio).getAttribute("aria-checked"), "true");
+		sleepSecond(2);
+	}
+
+	
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+}
