a3110de36f5e57a87930eef1de3e80e6e807bd72
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-07-28 17:26:52 +0700

    action

--- selenium/webdriver/Topic_14_Action.java
+++ selenium/webdriver/Topic_14_Action.java
@@ -1,15 +1,20 @@
 package webdriver;
 
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_00_Template_FF {
+public class Topic_14_Action {
 	WebDriver driver;
 	String projectPath = System.getProperty("user.dir");
+	Actions action;
 
 	public void sleepSecond(long timeSleep) {
 		try {
@@ -23,26 +28,40 @@ public class Topic_00_Template_FF {
 	public void beforeClass() {
 		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 		driver = new FirefoxDriver();
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		
+
+		action = new Actions(driver);
 	}
 
 	@Test
-	public void TC_01_() {
+	public void TC_01_Hover() {
+		driver.get("https://automationfc.github.io/jquery-tooltip/");
+		
+		action.moveToElement(driver.findElement(By.id("age"))).perform();
+		sleepSecond(3);
+		
+		//Verify
+		Assert.assertEquals(driver.findElement(By.className("ui-tooltip-content")).getText(), "We ask for your age only for statistical purposes.");
+		
 		
 	}
 
-	@Test
+//	@Test
 	public void TC_02_() {
 		
 	}
 
-	@Test
+//	@Test
 	public void TC_03_() {
 		
 	}
 
+//	@Test
+	public void TC_04_() {
+		
+	}
+	
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
