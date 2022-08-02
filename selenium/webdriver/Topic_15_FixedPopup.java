2d5abdde7200b8c014a51542fc5fb8c194deaaaa
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-08-02 17:23:52 +0700

    random popup

--- selenium/webdriver/Topic_15_FixedPopup.java
+++ selenium/webdriver/Topic_15_FixedPopup.java
@@ -1,5 +1,6 @@
 package webdriver;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -68,7 +69,7 @@ public class Topic_15_FixedPopup {
 		sleepSecond(1);
 	}
 
-	@Test
+//	@Test
 	public void TC_02_Random_Fixed_Popup() {
 		driver.get("https://www.kmplayer.com");
 		
@@ -83,8 +84,34 @@ public class Topic_15_FixedPopup {
 		System.out.println("Doing sth else");
 	}
 
-//	@Test
-	public void TC_03_() {
+	@Test
+	public void TC_03_Random_Not_in_DOM() {
+		driver.get("https://dehieu.vn/");
+		sleepSecond(2);
+		
+		List<WebElement> contentPopup = driver.findElements(By.cssSelector("div.popup-content"));
+		
+		if(contentPopup.size()>0) {
+			WebElement popupName = driver.findElement(By.id("popup-name"));
+			WebElement popupEmail = driver.findElement(By.id("popup-email"));
+			WebElement popupPhone = driver.findElement(By.id("popup-phone"));
+			
+			popupName.sendKeys("Khanh Cuong");
+			popupEmail.sendKeys("KC@gmail.com");
+			popupPhone.sendKeys("0987654321");
+			sleepSecond(3);
+			
+			driver.findElement(By.cssSelector("div.popup-content>button#close-popup")).click();
+			
+			
+			
+		}
+		
+		driver.findElement(By.xpath("//a[text()='Đăng nhập']")).click();
+		sleepSecond(2);
+		
+		//verify 
+		Assert.assertEquals(driver.getCurrentUrl(), "https://dehieu.vn/dang-nhap");
 		
 	}
 
