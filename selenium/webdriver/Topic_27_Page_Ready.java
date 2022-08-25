804c96eebca1d4b31e69ccb4d18430e9d16561b3
cuong.truong <cuong.truong@VN-MF10-NC100S7.LGE.NET>
2022-08-25 14:36:13 +0700

    Page ready

--- selenium/webdriver/Topic_27_Page_Ready.java
+++ selenium/webdriver/Topic_27_Page_Ready.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,29 +23,26 @@ public class Topic_27_Page_Ready {
 	WebDriverWait explicitWait;
 	JavascriptExecutor jsExecutor;
 	Actions action;
-	
+
 	public void sleepSecond(long timeSleep) {
 		try {
-			Thread.sleep(timeSleep*1000);
+			Thread.sleep(timeSleep * 1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	
+
 	@BeforeClass
 	public void beforeClass() {
-//		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
-//		driver = new FirefoxDriver();
-		
-		
-		
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+
 //		System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 //		driver = new ChromeDriver();
-		
-		System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
-		driver = new EdgeDriver();
-		
+//		
+//		System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+//		driver = new EdgeDriver();
+
 		action = new Actions(driver);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		explicitWait = new WebDriverWait(driver, 30);
@@ -53,88 +50,85 @@ public class Topic_27_Page_Ready {
 
 	}
 
-//	@Test
+	@Test
 	public void TC_01_OrangeHRM() {
 		driver.get("https://opensource-demo.orangehrmlive.com/index.php/dashboard");
-		
+
 		driver.findElement(By.id("txtUsername")).sendKeys("admin");
 		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
 		driver.findElement(By.id("btnLogin")).click();
-		
+
 		Assert.assertTrue(isPageLoadedSuccess());
-		
-		//verify
+
+		// verify
 		Assert.assertEquals(driver.findElement(By.cssSelector("div#task-list-group-panel-container span")).getText(), "3 month(s)");
-		
-		//click PIM
+
+		// click PIM
 		driver.findElement(By.cssSelector("a#menu_pim_viewPimModule>b")).click();
 		Assert.assertTrue(isPageLoadedSuccess());
-		
-		//input name
+
+		// input name
 		driver.findElement(By.id("empsearch_employee_name_empName")).sendKeys("Peter Mac");
 		driver.findElement(By.id("searchBtn")).click();
 		Assert.assertTrue(isPageLoadedSuccess());
-		
-		//verify
+
+		// verify
 		Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Peter Mac']")).isDisplayed());
-		
-		
+
 	}
 
-	@Test
+	// @Test
 	public void TC_02_TestProject() {
 		driver.get("https://blog.testproject.io/");
 		String keyword = "Selenium";
 
-		
-		//check popup
-		if(driver.findElement(By.cssSelector("div.mailch-wrap")).isDisplayed()) {
-			//close popup
+		// check popup
+		if (driver.findElement(By.cssSelector("div.mailch-wrap")).isDisplayed()) {
+			// close popup
 			driver.findElement(By.cssSelector("div.close-mailch")).click();
 		}
-		
+
 		WebElement searchForm = driver.findElement(By.cssSelector("section#search-2 input.search-field"));
-		//action.moveToElement(searchForm).perform();
-		
+		// action.moveToElement(searchForm).perform();
+
 		searchForm.sendKeys(keyword);
 		driver.findElement(By.cssSelector("section#search-2 span.glass")).click();
 		explicitWait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("h2.page-title")));
 		Assert.assertTrue(isPageLoadedSuccess());
-		
+
 		List<WebElement> allTitle = driver.findElements(By.cssSelector("h3.post-title"));
-		
+
 		for (WebElement postTitle : allTitle) {
 			String title = postTitle.getText();
-			System.out.println(title);	
+			System.out.println(title);
 			Assert.assertTrue(title.contains(keyword));
 		}
-		
-		
+
 	}
 
 //	@Test
 	public void TC_03_() {
-		
+
 	}
 
 //	@Test
 	public void TC_04_() {
-		
+
 	}
-	
+
 	public boolean isPageLoadedSuccess() {
 		explicitWait = new WebDriverWait(driver, 30);
 		jsExecutor = (JavascriptExecutor) driver;
-		
-		ExpectedCondition<Boolean>jQueryLoad = new ExpectedCondition<Boolean>() {
+
+		ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
 			@Override
 			public Boolean apply(WebDriver driver) {
 				return (Boolean) jsExecutor.executeScript("return (window.jQuery!= null) && (jQuery.active == 0);");
 			}
-			
+
 		};
-		
-		ExpectedCondition<Boolean>jsLoad = new ExpectedCondition<Boolean>() {
+
+		ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
 			@Override
 			public Boolean apply(WebDriver driver) {
 				return (Boolean) jsExecutor.executeScript("return document.readyState").toString().equals("complete");
@@ -142,7 +136,7 @@ public class Topic_27_Page_Ready {
 		};
 		return explicitWait.until(jQueryLoad) && explicitWait.until(jsLoad);
 	}
-	
+
 	@AfterClass
 	public void afterClass() {
 		driver.quit();
